c1fb7b173ae4ea756777404e6f73869c0df97b30
Priyanshu Sharma <59313375+priyanshu20@users.noreply.github.com>
2021-03-14 12:44:07 +0530

    feat(backend): Added boilerplate (#30)
    * feat(backend): Added boilerplate
    
    Added boiler plate for a flask api, using flask-restful and flask-cors
    
    * docs(flask-boilerplate) : Added necessary documentation for the boilerplate
    
    Made the changes suggested in the pr review
    
    * fix(curl-requrest)
    
    Replaced single quotes with double quotes

---
+++ server/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_restful import Api, Resource
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+DEBUG = False
+
+
+class TestRoute(Resource):
+    '''This is a mock route used for structuring the application'''
+
+    def get(self):
+        return {"message": "Welcome to the API!"}, 200
+
+    def post(self):
+        return{"message": "Welcome to the post route"}, 200
+
+
+api.add_resource(TestRoute, "/test")
+
+
+if __name__ == "__main__":
+    app.run(debug=DEBUG)
