65069c4eb329aeafdd21998d47015395ab01e180
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-04-05 13:07:12 +0530

    Added Landing Page (#55)
    Co-authored-by: bhubesh <bhubesh@localhost.localdomain>

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router , Switch , Route} from 'react-router-dom'
+
+import './App.css'
+import AboutUs from './components/AboutUs/AboutUs'
+// import Header from './components/Navbar/Header'
+import Organizations from './components/Organizations/Organizations'
+import Speakers from './components/Speakers/Speakers'
+import SpeakersOpinion from './components/SpeakersOpinion/SpeakersOpinion'
+// import Welcome from './components/WelcomePage/Welcome'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router className="App">
+  
+        {/* Header */}
+        {/* <Header></Header> */}
+    <Switch>
+      <Route path = '/'>
+        {/* welcome page */}
+
+        {/* <Welcome></Welcome> */}
+
+        {/* About us intro */}
+          <AboutUs></AboutUs>
+        {/* Featured Speakers */}
+          
+          <Speakers></Speakers>
+        {/* Orgaizations */}
+      
+          <Organizations></Organizations>
+          {/* Speakers opinion */}
+          <SpeakersOpinion></SpeakersOpinion>
+        
+        {/* Footer */}
+
+        {/* Contact us */}
+      </Route>
+    </Switch>
+  </Router>
+  )
 }
 
-export default App;
+export default App
+
+
+
